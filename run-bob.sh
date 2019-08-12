72967f5f4e423db85c8e8b2860a7a36096163f30
sappenin <sappenin@gmail.com>
2019-08-12 09:57:12 -0600

    Fixes #7
    Signed-off-by: sappenin <sappenin@gmail.com>

---
+++ run-bob.sh
@@ -0,0 +1,5 @@
+LEDGER_ADDRESS=rdVEU3RfkW4q9Fg3BTBhaNJr8gy9pZRrw \
+LEDGER_SECRET=shcuUXdrZKUvZXSvq3zuPxXRB5Hbs \
+ENGINE_PORT=9001 \
+CONNECTOR_URL=http://localhost:8081 \
+npm run-script start
--- src/controllers/accountsController.ts
+++ src/controllers/accountsController.ts
@@ -1,17 +1,24 @@
 import { Context } from 'koa'
-import { Redis } from 'ioredis'
 import { Account } from '../models/account'
+import { v4 as uuidv4 } from 'uuid'
 
 export async function create (ctx: Context) {
   let body = ctx.request.body
+
+  // If no accountId is specified, create a type4 UUID for the request.
+  let accountId = body.id ? body.id : uuidv4()
+
   let account: Account = {
-    id: body.id
+    id: accountId
   }
 
-    // Check if account exists first
+  // Check if account exists first
   const existingAccount = await ctx.redis.get(`${ctx.settlement_prefix}:accounts:${account.id}`)
-  if (!existingAccount) {
+  if (existingAccount) {
+    ctx.body = JSON.parse(existingAccount)
+  } else {
     await ctx.redis.set(`${ctx.settlement_prefix}:accounts:${account.id}`, JSON.stringify(account))
+    ctx.body = account
   }
 
   ctx.status = 200
@@ -33,5 +40,5 @@ export async function destroy (ctx: Context) {
   await ctx.redis.del(`${ctx.settlement_prefix}:accounts:${ctx.params.id}`)
     // TODO Delete key from redis for ledger address
 
-  ctx.status = 200
+  ctx.status = 204
 }

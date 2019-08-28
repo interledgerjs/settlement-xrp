3d883bf08c98723a73ac4df8fc7becebfbf3e7b0
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-08-28 18:38:32 -0400

    docs: move examples to separate folder

---
+++ examples/run-alice.sh
@@ -0,0 +1,4 @@
+LEDGER_SECRET=snH5tCJWYszQWRTBe61ssV617pzn6 \
+ENGINE_PORT=9000 \
+CONNECTOR_URL=http://localhost:8080 \
+npm run-script start
---
+++ examples/run-bob.sh
@@ -0,0 +1,4 @@
+LEDGER_SECRET=shcuUXdrZKUvZXSvq3zuPxXRB5Hbs \
+ENGINE_PORT=9001 \
+CONNECTOR_URL=http://localhost:8081 \
+npm run-script start

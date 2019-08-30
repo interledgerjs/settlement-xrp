786785b81c40e82dd4be1052dc60b72e057de5c7
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-08-30 17:59:02 -0400

    chore: prepare for publish to NPM
    - scripts now work on Windows
    - simplify scripts; circle config ensures everything runs before publish
    - simplify tsconfig
    - add publish to NPM to CI
    - fix executable
    - remove examples (not useful)

--- examples/run-alice.sh
+++
@@ -1,4 +0,0 @@
-LEDGER_SECRET=snH5tCJWYszQWRTBe61ssV617pzn6 \
-ENGINE_PORT=9000 \
-CONNECTOR_URL=http://localhost:8080 \
-npm run-script start
--- examples/run-bob.sh
+++
@@ -1,4 +0,0 @@
-LEDGER_SECRET=shcuUXdrZKUvZXSvq3zuPxXRB5Hbs \
-ENGINE_PORT=9001 \
-CONNECTOR_URL=http://localhost:8081 \
-npm run-script start
